a1d52323386b03daf12906faf61589c9e10e622f
Thiago Silva <thiagoclick2000@hotmail.com>
2024-02-04 09:09:52 -0300

    Casos de uso Cadastro de usuário

--- cypress/e2e/cadastro_usuario.cy.js
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -1,35 +1,53 @@
 /// <reference types = "cypress"/>
-
+import { faker } from '@faker-js/faker';
 import commum_page from '../support/pages/commum_page';
 import cadastro_page from '../support/pages/cadastro_usuario_page'
 
 describe('Cadastro de usuário', () => {
 
+    const randomName = faker.person.fullName(); 
+    const randomEmail =  faker.internet.email();
     beforeEach('acessar cadastro usuário',() => {
         commum_page.acessarCadastroUsuario()
     })
 
-    it.only('Campo nome vazio', () => {
+    it('Campo nome vazio', () => {
        cadastro_page.clicarCadastrar() 
        cadastro_page.validarMensagemErro("O campo nome deve ser prenchido" )
     });
 
     it('Campo Email vazio', () => {
-        
+        cadastro_page.preencherNome(randomName)
+        cadastro_page.clicarCadastrar()
+        cadastro_page.validarMensagemErro('O campo e-mail deve ser prenchido corretamente')
     });
     it('Campo Email inválido', () => {
-        
+        cadastro_page.preencherNome(randomName)
+        cadastro_page.preencherEmail(randomName)
+        cadastro_page.clicarCadastrar()
+        cadastro_page.validarMensagemErro('O campo e-mail deve ser prenchido corretamente')
     });
 
     it('Campo Senha  vazio', () => {
-        
+        cadastro_page.preencherNome(randomName)
+        cadastro_page.preencherEmail(randomEmail)
+        cadastro_page.clicarCadastrar()
+        cadastro_page.validarMensagemErro('O campo senha deve ter pelo menos 6 dígitos')
     });
     it('Campo Senha  inválido', () => {
-        
+        cadastro_page.preencherNome(randomName)
+        cadastro_page.preencherEmail(randomEmail)
+        cadastro_page.preencherSenha('12345')
+        cadastro_page.clicarCadastrar()
+        cadastro_page.validarMensagemErro('O campo senha deve ter pelo menos 6 dígitos')
     });
 
     it('Cadastro de usuário com sucesso', () => {
-        
+        cadastro_page.preencherNome(randomName)
+        cadastro_page.preencherEmail(randomEmail)
+        cadastro_page.preencherSenha('123456')
+        cadastro_page.clicarCadastrar()
+        cadastro_page.validarMensagemSucesso(randomName)
     });
 
     });
--- cypress/support/pages/cadastro_usuario_page.js
+++ cypress/support/pages/cadastro_usuario_page.js
@@ -17,5 +17,27 @@ clicarCadastrar(){
     cy.get('.errorLabel')
     .should('be.visible')
     .should('have.text', mensagem)
+   },
+
+   preencherNome(nome){
+    cy.get('#user')
+    .type(nome)
+   },
+   preencherEmail(email){
+    cy.get('#email')
+    .type(email)
+   },
+   preencherSenha(senha){
+    cy.get('#password')
+    .type(senha)
+   },
+
+   validarMensagemSucesso(nome){
+    cy.get('.swal2-title')
+    .should('have.text','Cadastro realizado!')
+    cy.get('.swal2-html-container')
+    .should('have.text',`Bem-vindo ${nome}`)
+
    }
+
 }
